17ac30b30181fa1d49f7b5079f38b3b2353f907a
Anshul-22103135 <22103135@mail.jiit.ac.in>
2024-07-25 16:31:22 +0530

    Updated Week1_Q3.cpp for Q4

--- Week1_Q3.cpp
+++ Week1_Q3.cpp
@@ -1,3 +1,4 @@
+// reverse the inputed string
 #include<iostream>
 using namespace std;
 
@@ -8,6 +9,7 @@ int main()
     getline(cin,ss);
     int n = ss.length();
     int s =0,e=n-1;
+    //Approach - Using the two pointer approach to get the string reversed
     while(s<=e)
     {
         swap(ss[s],ss[e]);
